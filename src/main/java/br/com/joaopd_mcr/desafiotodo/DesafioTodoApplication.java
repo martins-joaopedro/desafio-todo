1a4ffde8c75ee4f020df004ac73b635033d9f6f3
João Pedro Martins Cruz <103667165+martins-joaopedro@users.noreply.github.com>
2024-01-13 16:00:07 -0300

    Initial commit

---
+++ src/main/java/br/com/joaopd_mcr/desafiotodo/DesafioTodoApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaopd_mcr.desafiotodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaopd_mcr/desafiotodo/DesafioTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaopd_mcr.desafiotodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
