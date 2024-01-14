03b98b01ea171ab4e4a6bf0d018e6a402a5ed366
João Pedro Martins Cruz <103667165+martins-joaopedro@users.noreply.github.com>
2024-01-14 14:28:38 -0300

    cors config

---
+++ src/main/java/br/com/joaopd_mcr/desafiotodo/config/webConfig.java
@@ -0,0 +1,15 @@
+package br.com.joaopd_mcr.desafiotodo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class webConfig implements WebMvcConfigurer {
+    
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
